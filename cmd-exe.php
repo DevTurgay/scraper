c40513ae89dade3e5f57b26650a3c2dcb7a56077
DevTurgay <dev.turgay.ali@gmail.com>
2022-05-30 06:53:24 +0400

    remove unneded

--- cmd-exe.php
+++ cmd-exe.php
@@ -6,7 +6,6 @@ require 'vendor/autoload.php';
 
 if (isset($argv[1]) && !empty($argv[1])) {
 	$scraper = new \Scraper\ScraperController;
-	echo '<pre>';
 	print_r(
 		$scraper
 			->searchKeyWord(HOME, $argv[1])
